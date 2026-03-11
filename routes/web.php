7bfa1e379e4ff315ae94cb1f246e21a981e0bc56
XDree <ivan.lebedev5454@gmail.com>
2026-03-11 22:00:24 +0300

    Update Site

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('components.login_page');
+});
+
+Route::get('/home', function () {
+    return view('components.home_page');
 });
